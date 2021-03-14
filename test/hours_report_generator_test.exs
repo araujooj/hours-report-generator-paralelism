e3a8e7947b40bd1795b90923d57cbd3623777e7d
Gabriel Araujo <gabriel@kenzie.com.br>
2021-03-14 11:59:08 -0300

    base project

---
+++ test/hours_report_generator_test.exs
@@ -0,0 +1,8 @@
+defmodule HoursReportGeneratorTest do
+  use ExUnit.Case
+  doctest HoursReportGenerator
+
+  test "greets the world" do
+    assert HoursReportGenerator.hello() == :world
+  end
+end
