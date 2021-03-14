4274530bf9a1957cbbe9f66399ada93af8778ac5
Gabriel Araujo <gabriel@kenzie.com.br>
2021-03-14 13:33:41 -0300

    feature: added tasks management on project, receiving a list of files

--- lib/hours_report_generator.ex
+++ lib/hours_report_generator.ex
@@ -7,19 +7,29 @@ defmodule HoursReportGenerator do
     |> Enum.reduce(report_acc(), fn line, report -> sum_values(line, report) end)
   end
 
+  def build_from_many(filenames) when not is_list(filenames) do
+    {:error, "Provide a list of files"}
+  end
+
+  def build_from_many(filenames) do
+    filenames
+    |> Task.async_stream(&build/1)
+    |> Enum.reduce(report_acc(), fn {:ok, result}, _report -> {:ok, result} end)
+  end
+
   defp sum_values([username, hours, _day, month, year], %{
          "all_hours" => all_hours,
          "hours_per_month" => hours_per_month,
          "hours_per_year" => hours_per_year
        }) do
     all_hours = merge_maps(all_hours, %{username => hours})
-    hours_per_month = merge_year_and_month_maps(hours_per_month, %{username => %{month => hours}})
-    hours_per_year = merge_year_and_month_maps(hours_per_year, %{username => %{year => hours}})
+    hours_per_month = merge_sub_maps(hours_per_month, %{username => %{month => hours}})
+    hours_per_year = merge_sub_maps(hours_per_year, %{username => %{year => hours}})
 
     build_report(all_hours, hours_per_month, hours_per_year)
   end
 
-  defp merge_year_and_month_maps(map1, map2) do
+  defp merge_sub_maps(map1, map2) do
     Map.merge(map1, map2, fn _k, v1, v2 -> merge_maps(v1, v2) end)
   end
 
